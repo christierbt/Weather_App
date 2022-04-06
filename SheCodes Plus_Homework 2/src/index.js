acb9def33b99e8106d912b044057a375bed52fa9
christierbt <98954266+christierbt@users.noreply.github.com>
2022-04-07 06:15:33 +0800

    Imported Project

---
+++ SheCodes Plus_Homework 2/src/index.js
@@ -0,0 +1,86 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+let currentDate = document.querySelector("#current-date");
+let currentTime = new Date();
+currentDate.innerHTML = formatDate(currentTime);
+
+// convert to fahrenheit
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temp");
+  let currentTemp = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = Math.round((currentTemp * 9) / 5 + 32);
+}
+
+let fahreinheitLink = document.querySelector("#fahrenheit-link");
+fahreinheitLink.addEventListener("click", convertToFahrenheit);
+
+// weather update via search button
+function search(city) {
+  let apiKey = "4b433d9a7f22e9aa26d3e1760e73b0a6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  search(city);
+}
+
+function currentTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+// current location weather
+
+function searchLocation(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+  let apiKey = "4b433d9a7f22e9aa26d3e1760e73b0a6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+function displayCurrentData(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", displayCurrentData);
+
+search("Perth");
